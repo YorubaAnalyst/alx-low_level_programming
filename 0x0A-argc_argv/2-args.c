80474712e687a27606a859ead58603155b2347f8
YorubaAnalyst <stemidayo320gmail.com>
2023-04-09 23:19:00 -0700

    argv

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -2,28 +2,21 @@
 #include "main.h"
 
 /**
- * main - prints all the arguments it receives
+ * main - prints all arguments it receives
  * @argc: number of arguments
  * @argv: array of arguments
  *
  * Return: Always 0 (Success)
  */
-
 int main(int argc, char *argv[])
 {
+	int i;
 
-/*Declaring variables*/
-int count = 0;
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
 
-if (arg > 0)
-{
-/*WHILE - print each arguments*/
-while (count < argc)
-{
-printf("%s\n", argv[count]);
-count++;
-}
-}
-return (0);
+	return (0);
 }
 
